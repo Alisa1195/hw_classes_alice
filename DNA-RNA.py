6ea3ad6877737f024178a6a908b80ef2c833940d
Alisa1195 <1195alisa@gmail.com>
2018-11-11 20:53:41 +0300

    unnecessary "self" prefixes have been eliminated

--- DNA-RNA.py
+++ DNA-RNA.py
@@ -8,18 +8,18 @@ class RNA:
         self.complementation = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C'}
 
     def gc(self):
-        self.gc_count = 0
+        gc_count = 0
         for i in range(self.length):
             if (self.seq[i] == 'G') or (self.seq[i] == 'C'):
-                self.gc_count += 1
-        self.gc_content = str(round(((self.gc_count / self.length) * 100), 1)) + str('%')
-        return (self.gc_content)
+                gc_count += 1
+        gc_content = str(round(((gc_count / self.length) * 100), 1)) + str('%')
+        return (gc_content)
 
     def reverse_complement(self):
-        self.complement_seq = str()
+        complement_seq = str()
         for i in self.seq:
-            self.complement_seq += self.complementation[i]
-        return (self.complement_seq[::-1])
+            complement_seq += self.complementation[i]
+        return (complement_seq[::-1])
 
 
 class DNA(RNA):
@@ -29,10 +29,10 @@ class DNA(RNA):
         self.complementation = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
     def transcribe(self):
-        self.matrix = self.seq.replace('T', 'U')
-        self.transcript_complement = RNA(self.matrix)
-        self.transcript = self.transcript_complement.reverse_complement()
-        return (self.transcript)
+        matrix = self.seq.replace('T', 'U')
+        transcript_complement = RNA(matrix)
+        transcript = transcript_complement.reverse_complement()
+        return (transcript[::-1])
 
 
 
